301e59b648dd23b2067d5dc5b540790fe37853bb
Dheraj K <dherajkd2@gmail.com>
2026-04-25 23:26:36 +0530

    Greater Element Than X code is pushed

--- GreaterElementThanX/GreaterElementThanX.cpp
+++ GreaterElementThanX/GreaterElementThanX.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 using namespace std;
 
+// function returns count of elements > X
+int countGreater(int arr[], int n, int x) {
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > x) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
 int main() {
-    
+
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int x;
+    cout << "Enter X: ";
+    cin >> x;
+
+    int result = countGreater(arr, n, x);
+
+    cout << "Count of elements greater than " << x << " = " << result << endl;
+
+    return 0;
 }
--- GreaterElementThanX/GreaterElementThanX.java
+++ GreaterElementThanX/GreaterElementThanX.java
@@ -1,10 +1,41 @@
 package GreaterElementThanX;
+import java.util.Scanner;
 
-public class GreaterElementThanX {
-    public static void main(String[] args) {
-        int arr[]={5,3,4,11,9,2};
-        System.out.println("Hello");
-        
+class GreaterElement {
+
+    // function returns count of elements > X
+    static int countGreater(int[] arr, int x) {
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > x) {
+                count++;
+            }
+        }
+
+        return count;
     }
-   
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter X: ");
+        int x = sc.nextInt();
+
+        int result = countGreater(arr, x);
+
+        System.out.println("Count of elements greater than " + x + " = " + result);
+    }
 }
