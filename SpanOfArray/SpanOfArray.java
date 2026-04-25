513d3df72653ff06c6537b720bb235e346bbb2d6
Dheraj K <dherajkd2@gmail.com>
2026-04-25 22:53:56 +0530

    Pushing the code Of Span Of Array

--- SpanOfArray/SpanOfArray.java
+++ SpanOfArray/SpanOfArray.java
@@ -1,5 +1,19 @@
 package SpanOfArray;
-
 public class SpanOfArray {
-    
-}
+    public static void main(String[] args) {
+        int arr[]={5,3,4,11,9,2};
+
+        int max=arr[0];
+        int min=arr[0];
+        int span=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max)max=arr[i];
+            if(arr[i]<min)min=arr[i];
+        }
+            span = max - min;
+            System.out.println("Max : "+max);
+            System.out.println("Min : "+min);
+            System.out.println("Span Of Array :"+span);
+        }
+    }
+
